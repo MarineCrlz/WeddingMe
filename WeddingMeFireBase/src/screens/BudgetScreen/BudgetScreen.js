4cd3570eb83a36cc40a2d7ed893d6f3b21f546d9
MarineCrlz <marine.carliez@gmail.com>
2021-03-25 21:45:21 +0100

    Test navigateur reussi

--- WeddingMeFireBase/src/screens/BudgetScreen/BudgetScreen.js
+++ WeddingMeFireBase/src/screens/BudgetScreen/BudgetScreen.js
@@ -3,11 +3,20 @@ import { Text, View, Image } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import styles from './styles';
 
-export default function BudgetScreen(props) {
+export default function BudgetScreen({navigation}) {
+
+    const onLoginPress = () => {
+        navigation.navigate('Home')
+    }
 
     return (
         <View style={styles.container}>
             <Text>Mon budget</Text>
+            <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => onLoginPress()}>
+                    <Text style={styles.buttonTitle}>Touche moi</Text>
+                </TouchableOpacity>
         </View>
     )
 }
