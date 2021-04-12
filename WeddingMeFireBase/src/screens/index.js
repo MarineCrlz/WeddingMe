e7fc54efebae4d8c8ff83a8d51561159f5872e08
MarineCrlz <marine.carliez@gmail.com>
2021-04-12 20:29:31 +0200

    Maj routes

--- WeddingMeFireBase/src/screens/index.js
+++ WeddingMeFireBase/src/screens/index.js
@@ -4,6 +4,4 @@ export { default as HomeScreen } from './HomeScreen/HomeScreen'
 
 export { default as RegistrationScreen } from './RegistrationScreen/RegistrationScreen'
 
-export { default as MenuScreen } from './MenuScreen/MenuScreen'
-
 export { default as AccountScreen } from './AccountScreen/AccountScreen'
