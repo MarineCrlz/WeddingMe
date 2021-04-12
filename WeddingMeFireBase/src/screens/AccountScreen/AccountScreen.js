480a4c19e7e37985b2e4bf9ddbc36eab8d3c5e77
MarineCrlz <marine.carliez@gmail.com>
2021-04-12 20:27:20 +0200

    Page terminee (reste un pb technique : on doit changer tous les champs en meme temps, sera traite en bonus)

--- WeddingMeFireBase/src/screens/AccountScreen/AccountScreen.js
+++ WeddingMeFireBase/src/screens/AccountScreen/AccountScreen.js
@@ -6,7 +6,8 @@ import { firebase } from '../../firebase/config'
 import { useEffect, componentDidMount } from 'react';
 
 export default function AccountScreen({navigation}) {
-    const [userList, setUsers] = useState([])
+    //Variables contenant les actuellement en mémoire dans FireBase
+    const [uid, setID] = useState('')
     const [userName, setName] = useState('')
     const [userPartner, setPartner] = useState('')
     const [userBudget, setBudget] = useState('')
@@ -14,6 +15,13 @@ export default function AccountScreen({navigation}) {
     const [userDate, setDate] = useState('')
     const [userMail, setMail] = useState('')
 
+    //Variables vides, utilisées pour conserver les valeurs initiales en mémoire
+    const[name, _setName] = useState('')
+    const [partner, _setPartner] = useState('')
+    const [budget, _setBudget] = useState('')
+    const [venue, _setVenue] = useState('')
+    const [date, _setDate] = useState('')
+    const [mail, _setMail] = useState('')
 
     //Recuperation du user
     firebase.
@@ -25,6 +33,10 @@ export default function AccountScreen({navigation}) {
                 //console.log('User exists: ', firestoreDocument.exists);
                 if (firestoreDocument.exists) {
                     const user = firestoreDocument.data()
+                    setID(user.id)
+                    console.log("Donne infos id")
+                    console.log(user.id)
+                    console.log(uid)
                     setName(user.fullName)
                     setPartner(user.partner)
                     setBudget(user.budget)
@@ -33,6 +45,67 @@ export default function AccountScreen({navigation}) {
                     setMail(user.email)
                 }
             });
+
+    //ATTENTION ici modification temporaire du code : on reprend systématiquement les news valeurs
+    //Besoin donc que tout soit rempli à chaque fois
+    const sendData = () => {
+        updateData()
+        firebase.
+            firestore()
+                .collection('users')
+                .doc(uid)
+                .update ({
+                    date : date,
+                    email : mail,
+                    fullName : name,
+                    partner : partner,
+                    venue : venue,
+                });
+        console.log('Quelles donnees sont envoyées ?')
+        console.log(userDate)
+        console.log(userMail)
+        console.log(userName)
+        console.log(userPartner)
+        console.log(userVenue)
+    }
+
+    //Pour éviter d'avoir des champs vides
+    //ATTENTION modification du champ dans la boucle if ne fonctionne pas ?...
+    const updateData = () => {
+        if (name !== "") {
+            setName(name)
+            console.log(userName)
+            console.log("Name non nul")
+            console.log(name)
+            console.log(userName)
+        }
+        if (partner !== "") {
+            setPartner(partner)
+            console.log("partner non nul")
+            console.log(partner)
+            console.log(userPartner)
+        }
+        if (budget !== "") {
+            setBudget(budget)
+            console.log("budget non nul")
+            console.log(budget)
+        }
+        if (venue !== "") {
+            setVenue(venue)
+            console.log("venue non nul")
+            console.log(venue)
+        }
+        if (date !== "") {
+            setDate(date)
+            console.log("date non nul")
+            console.log(date)
+        }
+        if (mail !== "") {
+            setMail(mail)
+            console.log("mail non nul")
+            console.log(mail)
+        }
+    }
 
     //On récupère les données actuellement stockées
     // useEffect(() => {
@@ -66,6 +139,11 @@ export default function AccountScreen({navigation}) {
             <View style={styles.header}>
                 <Text>Mon compte</Text>
             </View>
+            <View //ATTENTION RAJOUT DE CODE ICI
+            >
+                <Text>
+                    Attention, si vous voulez modifier un champ, il vous est demandé de renoter également l'ensemble des champs présents.</Text>
+            </View>
             <View style={styles.donnees}>
                 <View style={styles.donneesSpe}>
                     <Text>Prénom</Text>
@@ -73,6 +151,8 @@ export default function AccountScreen({navigation}) {
                         style={styles.input}
                         placeholder={JSON.stringify(userName)}
                         placeholderTextColor="#aaaaaa"
+                        onChangeText={(text) => _setName(text)}
+                        value={name}
                         //onChangeText={(text) => this.keepTextInputChanged(text,fullName)}
                     />
                 </View>
@@ -82,6 +162,8 @@ export default function AccountScreen({navigation}) {
                         style={styles.input}
                         placeholder={JSON.stringify(userPartner)}
                         placeholderTextColor="#aaaaaa"
+                        value={partner}
+                        onChangeText={(text) => _setPartner(text)}
                         //onChangeText={(text) => this.keepTextInputChanged(text,fullName)}
                     />
                 </View>
@@ -91,6 +173,8 @@ export default function AccountScreen({navigation}) {
                         style={styles.input}
                         placeholder={JSON.stringify(userBudget)}
                         placeholderTextColor="#aaaaaa"
+                        value={budget}
+                        onChangeText={(text) => _setBudget(text)}
                         //onChangeText={(text) => this.keepTextInputChanged(text,fullName)}
                     />
                 </View>
@@ -100,6 +184,8 @@ export default function AccountScreen({navigation}) {
                         style={styles.input}
                         placeholder={JSON.stringify(userVenue)}
                         placeholderTextColor="#aaaaaa"
+                        value={venue}
+                        onChangeText={(text) => _setVenue(text)}
                         //onChangeText={(text) => this.keepTextInputChanged(text,fullName)}
                     />
                 </View>
@@ -109,6 +195,8 @@ export default function AccountScreen({navigation}) {
                         style={styles.input}
                         placeholder={JSON.stringify(userDate)}
                         placeholderTextColor="#aaaaaa"
+                        value={date}
+                        onChangeText={(text) => _setDate(text)}
                         //onChangeText={(text) => this.keepTextInputChanged(text,fullName)}
                     />
                 </View>
@@ -118,6 +206,8 @@ export default function AccountScreen({navigation}) {
                         style={styles.input}
                         placeholder={JSON.stringify(userMail)}
                         placeholderTextColor="#aaaaaa"
+                        value={mail}
+                        onChangeText={(text) => _setMail(text)}
                         //onChangeText={(text) => this.keepTextInputChanged(text,fullName)}
                     />
                 </View>
@@ -125,6 +215,7 @@ export default function AccountScreen({navigation}) {
             <TouchableOpacity
                     style={styles.button}
                     //onPress={() => onLoginPress()}
+                    onPress={() => sendData()}
                     >
                     <Text style={styles.buttonTitle}>Modifier</Text>
             </TouchableOpacity>
